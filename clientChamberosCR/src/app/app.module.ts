7bdcf2d5f341a7896a0467be7e343f6cc3c9fb9d
Wilmer <vvilmervd@gmail.com>
2018-09-15 11:28:37 -0600

    login,routes,services and libraries

--- clientChamberosCR/src/app/app.module.ts
+++ clientChamberosCR/src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import {FormsModule} from '@angular/forms'; 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './component/login/login.component';
+import { app_routing }from "./app.routes";
+import { Routes,RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import {HttpModule} from "@angular/http";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    app_routing,
+    HttpClientModule,
+    FormsModule,
+    RouterModule,
+    ReactiveFormsModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
