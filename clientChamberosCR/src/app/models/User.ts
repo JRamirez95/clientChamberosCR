95980ca94ea500e00ffe949bdf0eae1466545357
Wilmer <vvilmervd@gmail.com>
2018-09-19 14:48:14 -0600

    models

---
+++ clientChamberosCR/src/app/models/User.ts
@@ -0,0 +1,14 @@
+export class User{
+    _id?: string;
+    name: string;
+    surnames: string;
+    age: string;
+    gender:string;
+    email: string;
+    birthdate: Date;
+    password: string;
+    latitud: string;
+    longitud: string;
+    approvalstatus: string;
+   
+}
