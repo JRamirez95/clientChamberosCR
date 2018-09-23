fb8965bbff405e9c222fbbcd2e651ad6b509909a
Jonathan Ramírez <jona16121995@gmail.com>
2018-09-22 20:26:26 -0600

    Varios

--- clientChamberosCR/src/app/app.routes.ts
+++ clientChamberosCR/src/app/app.routes.ts
@@ -4,13 +4,16 @@ import { RegisterComponent } from './component/register/register.component';
 import { BuscarChamberosComponent } from './component/buscar-chamberos/buscar-chamberos.component';
 import { HomeComponent } from './component/home/home.component';
 import { SearchChamberosComponent } from './component/search-chamberos/search-chamberos.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
 
 const app_routes: Routes = [
   {path:'login', component: LoginComponent},
   {path:'register', component: RegisterComponent},
   {path:'buscar-chamberos', component: BuscarChamberosComponent},
+  {path:'', component: HomeComponent},
   {path:'home', component: HomeComponent},
-  {path:'search', component: SearchChamberosComponent}
+  {path:'search', component: SearchChamberosComponent},
+  {path:'dashboard', component: DashboardComponent}
     
 ];
   
