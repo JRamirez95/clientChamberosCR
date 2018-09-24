e10307935751310f443731952525e237d6e7b391
Wilmer <vvilmervd@gmail.com>
2018-09-24 16:36:45 -0600

    dashboard

--- clientChamberosCR/src/app/component/update-user/update-user.component.ts
+++ clientChamberosCR/src/app/component/update-user/update-user.component.ts
@@ -1,15 +1,57 @@
 import { Component, OnInit } from '@angular/core';
+import {UserService} from '../../services/user/user.service';
+import {User} from '../../models/User';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-update-user',
   templateUrl: './update-user.component.html',
-  styleUrls: ['./update-user.component.css']
+  styleUrls: ['./update-user.component.css'],
+  providers: [UserService]
 })
 export class UpdateUserComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userServices: UserService, private router:Router) { }
+  user = new User();
 
   ngOnInit() {
   }
+
+  /*getUser(){
+      
+    var userToken = localStorage.getItem("userToken");
+        this.userServices.(userToken)
+        .subscribe(user => {
+        console.log(user);
+        this.user = user;
+        });
+      }*/
+
+  editUser(event) {
+    event.preventDefault();
+        var id = this.user._id;
+        var userToken = localStorage.getItem("userToken");
+        this.userServices.editUser(this.user,id, userToken)
+            .subscribe(user => {
+                user =  user;
+                alert("Edit exitoso");
+            });
+}
+
+deleteUser(event) {
+    event.preventDefault();
+    
+    
+        var userToken = localStorage.getItem("userToken");
+        var id = this.user._id;
+
+        
+        this.userServices.deleteUser(id, userToken)
+            .subscribe(user => {
+                console.log(this.user);
+                user =  user;
+                alert("Delete exitoso");
+            });
+}
 
 }
