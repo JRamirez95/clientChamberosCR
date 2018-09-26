9b6befd6af20d2e961646fd6a1f236bdeec22afa
Wainercrb <2011-peswainer.1996@gmail.com>
2018-09-25 23:39:38 -0600

    updated angular routes

--- clientChamberosCR/src/app/app.routes.ts
+++ clientChamberosCR/src/app/app.routes.ts
@@ -15,8 +15,8 @@ const app_routes: Routes = [
   {path:'home', component: HomeComponent},
   {path:'search', component: SearchChamberosComponent},
   {path:'dashboard', component: DashboardComponent},
-
-  {path:'search/:location/:km', component: SearchChamberosComponent},
+  
+  {path:'search', component: SearchChamberosComponent},
 
   {path:'updateUser', component: UpdateUserComponent}
 
